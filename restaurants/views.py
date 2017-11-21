8408bed52fb292953164aa9df5a38cc1d058ed43
athanasiouconst <csiouath@gmail.com>
2017-11-21 15:39:28 +0200

    template inheritance

--- restaurants/views.py
+++ restaurants/views.py
@@ -22,5 +22,31 @@ def home_old(request):
 	#return render (request, "home.html", {}) #response
 
 def home(request):
-	num = random.randint(0,10000000)
-	return render (request, "base.html", {"html_var":"Django", "num":num}) #response
+	html_var = 'Django'
+	num=None
+	
+	some_list = [random.randint(0,10000000), random.randint(0,10000000), random.randint(0,10000000)]
+	condition_bool_item  = True
+
+	if condition_bool_item:
+		num = random.randint(0,10000000)
+	context = {
+				"html_var":html_var, 
+				"num":num,
+				"some_list":some_list
+				}
+	return render (request, "home.html", context)  #response
+
+def about(request):
+	html_var = 'Django'
+	context = {
+				"html_var":html_var
+				}
+	return render (request, "about.html", context)  #response
+
+def contact(request):
+	html_var = 'Django'
+	context = {
+				"html_var":html_var
+				}
+	return render (request, "contact.html", context)  #response
