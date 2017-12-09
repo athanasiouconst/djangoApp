dec4696e11054800e53dd9fc13ef9ee5fcaa299e
athanasiouconst <csiouath@gmail.com>
2017-12-09 20:19:18 +0200

    query set at models

--- djangoApp/urls.py
+++ djangoApp/urls.py
@@ -18,11 +18,22 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 #from restaurants.views import HomeView , AboutView, ContactView 
 #home, about, contact, ContactView, AboutView
-from restaurants.views import restaurant_listview
+from restaurants.views import (
+    restaurant_listview,
+    RestaurantListView
+    #SearchRestaurantListView
+    #,
+    #MexicanRestaurantListView,
+    #AsianFusionRestaurantListView
+    )
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^restaurants$', restaurant_listview),
+    url(r'^restaurants$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    #url(r'^restaurants/asian$', AsianFusionRestaurantListView.as_view()),
     url(r'^about$', TemplateView.as_view(template_name='about.html')),
     #url(r'^contact$', ContactTemplateView.as_view()),
      url(r'^contact$', TemplateView.as_view(template_name='contact.html')),
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,18 +1,56 @@
 import random
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .models import RestaurantLocation
 
-def restaurant_listview(request):
-	template_name='restaurants/restaurants_list.html'
-	queryset = RestaurantLocation.objects.all()
-	context={
-		"object_list" : queryset
-	}
+def restaurant_listview(request,):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
 
-	return render(request,template_name,context)
+
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug)
+                )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
+
+
+# class SearchRestaurantListView(ListView):
+# 	template_name='restaurants/restaurants_list.html'
+
+# 	def get_queryset(self):
+# 		print(self.kwargs)
+# 		slug = self.kwargs.get("slug")
+# 		if slug:
+# 			queryset = RestaurantLocation.objects.filter(
+# 					Q(category__iexact=slug) |
+# 					Q(category__icontains=slug)
+# 				)
+# 		else:
+# 			queryset = RestaurantLocation.objects.none
+# 		return queryset
+
+# class MexicanRestaurantListView(ListView):
+# 	queryset = RestaurantLocation.objects.filter(category__iexact='Mexican')
+# 	template_name='restaurants/restaurants_list.html'
+
+# class AsianFusionRestaurantListView(ListView):
+# 	queryset = RestaurantLocation.objects.filter(category__iexact='Asian Fusion')
+# 	template_name='restaurants/restaurants_list.html'
+
 
 # # Create your views here.
 # def home_old(request):
