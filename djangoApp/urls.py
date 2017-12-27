5f9176c662d4f6f71f83c915c7cf5358825df569
athanasiouconst <csiouath@gmail.com>
2017-12-27 15:43:28 +0200

    model - view detail form

--- djangoApp/urls.py
+++ djangoApp/urls.py
@@ -32,12 +32,12 @@ from restaurants.views import (
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^restaurants$', RestaurantListView.as_view()),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
     #url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
     url(r'^restaurants/(?P<rest_id>\w+)/$', RestaurantDetailView.as_view()),
     #url(r'^restaurants/asian$', AsianFusionRestaurantListView.as_view()),
-    url(r'^about$', TemplateView.as_view(template_name='about.html')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     #url(r'^contact$', ContactTemplateView.as_view()),
-     url(r'^contact$', TemplateView.as_view(template_name='contact.html')),
+     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
     # url(r'^contact/(?P<id>\d+)$', ContactView.as_view()),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,7 +1,7 @@
-import random
+
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import RestaurantLocation
@@ -14,15 +14,14 @@ def restaurant_listview(request,):
     }
     return render(request, template_name, context)
 
-
 class RestaurantListView(ListView):
     def get_queryset(self):
         slug = self.kwargs.get("slug")
         if slug:
             queryset = RestaurantLocation.objects.filter(
-                    Q(category__iexact=slug) |
-                    Q(category__icontains=slug)
-                )
+                Q(category__iexact=slug) |
+                Q(category__icontains=slug)
+            )
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
@@ -30,7 +29,7 @@ class RestaurantListView(ListView):
 
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
-    
+
     def get_object(self, *args, **kwargs):
         rest_id = self.kwargs.get('rest_id')
         obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
@@ -39,7 +38,7 @@ class RestaurantDetailView(DetailView):
 
 
 
-# class SearchRestaurantListView(ListView):
+        # class SearchRestaurantListView(ListView):
 # 	template_name='restaurants/restaurants_list.html'
 
 # 	def get_queryset(self):
